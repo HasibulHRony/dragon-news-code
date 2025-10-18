689bbbb16bd3bbe66114068ee21e9b2eecff9032
HasibulHRony <hasibulhrony7@gmail.com>
2025-10-18 14:34:25 +0600

    navbar and news headline added.

--- src/Layouts/HomeLayout.jsx
+++ src/Layouts/HomeLayout.jsx
@@ -1,16 +1,27 @@
 import React from 'react'
 import { Outlet } from 'react-router'
 import { Header } from '../Components/Header/Header'
+import { LatestNews } from '../Components/LatestNews/LatestNews'
+import { NavBar } from '../Components/NavBar/NavBar'
 
 export const HomeLayout = () => {
   return (
     <div>
+      <header>
         <Header></Header>
-        <main>
-            <section className='left_nav'><h2>LeftNavbar</h2></section>
-            <section className='main'><Outlet></Outlet></section>
-            <section className='right_nav'><h2>RightNavbar</h2></section>
-        </main>
+        <section className='w-11/12 mx-auto my-3'>
+          <LatestNews></LatestNews>
+          <nav  className='w-11/12 mx-auto my-3'>
+            <NavBar></NavBar>
+          </nav>
+        </section>
+
+      </header>
+      <main>
+        <section className='left_nav'></section>
+        <section className='main'><Outlet></Outlet></section>
+        <section className='right_nav'></section>
+      </main>
     </div>
   )
 }
